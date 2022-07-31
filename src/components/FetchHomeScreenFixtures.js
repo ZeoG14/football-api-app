a6a742c85b7442a810e6e078274fa54640abb05f
Zeog14 <zeog14@gmail.com>
2022-07-31 11:54:28 -0500

    correctly configured api call

--- src/components/FetchHomeScreenFixtures.js
+++ src/components/FetchHomeScreenFixtures.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from 'react'
+import styled from "styled-components"
 import axios from 'axios'
 
 const FetchHomeScreenFixtures = () => {
@@ -19,14 +20,13 @@ const FetchHomeScreenFixtures = () => {
     }, [])
 
     if(fixtures){
+        console.log(fixtures)
         return(
             <div className='fixtures'>
-                <h2>Standings</h2>
-                {fixtures.standings.map((data) =>
-                <div>
-                        {data.team.displayName}
-                </div>
-                )}
+                <Table>
+                <TableHeader>
+                </TableHeader>
+                </Table>
             </div>
         )
     }
@@ -38,3 +38,29 @@ const FetchHomeScreenFixtures = () => {
 }
 
 export default FetchHomeScreenFixtures
+
+
+const Table = styled.div`
+  letter-spacing: .02em;
+  display: flex;
+  flex-direction: column;
+`
+const TableHeader = () =>
+  <div style={{ display: "flex", flexDirection: "row-reverse" }}>
+    <Th>Pl</Th>
+    <Th>W</Th>
+    <Th>D</Th>
+    <Th>L</Th>
+    <Th>GF</Th>
+    <Th>GA</Th>
+    <Th>GD</Th>
+    <Th>Pts</Th>
+  </div>
+
+const Th = styled.div`
+  width: 2em;
+  padding: .5em;
+  border: solid #360037 1px;
+  border-right: 0;
+  font-weight: 400;
+`
